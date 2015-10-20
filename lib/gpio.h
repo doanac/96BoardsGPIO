d8763468e62262749069d4adfe426e6ec9552a13
Andy Doan <andy.doan@linaro.org>
2015-10-20 16:40:18 -0500

    convert to use libsoc
    This converts the project to require the libsoc project and minimizes
    our API.
    
    Signed-off-by: Andy Doan <andy.doan@linaro.org>

--- lib/gpio.h
+++ lib/gpio.h
@@ -10,25 +10,23 @@
  * and/or modify it under the terms of the GNU Lesser General Public License
  * as published by the Free Software Foundation; either version 2.1 of the
  * License.
- * 
- * This library is distributed in the hope that it will be useful, 
+ *
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public 
- * License along with this library; if not, write to the Free Software 
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  *
  ***********************************************************************
  */
- 
+
 #define HIGH 1
 #define LOW 0
 
-extern int init_96Boards_GPIO_library(char * );
-extern int open_GPIO_Board_pin_number( int );
-extern int close_GPIO( int );
-extern int setup_GPIO( int, char * );
-extern int digitalRead( int );
-extern int digitalWrite( int, int );
+unsigned int gpio_id(const char *pin_name);
+int gpio_open(unsigned int gpio_id, const char *direction);
+int digitalRead(unsigned int gpio_id);
+int digitalWrite(unsigned int gpio_id, unsigned int value);
